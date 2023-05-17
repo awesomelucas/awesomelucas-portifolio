0422d2935529c76c11fc3ca01874fc7d641d4595
awesomelucas <lucascostaschmidt@gmail.com>
2023-05-17 10:30:52 -0300

    add new stacks

--- src/utils/stackData.ts
+++ src/utils/stackData.ts
@@ -4,36 +4,44 @@ import imgJS from "../public/static/img/stack/js.svg";
 import imgNode from "../public/static/img/stack/node.svg";
 import imgStyled from "../public/static/img/stack/styled.svg";
 import imgReact from "../public/static/img/stack/react.svg";
-import { FaVuejs } from "react-icons/fa";
+import { FaPython } from "react-icons/fa";
+import { FaHtml5 } from "react-icons/fa";
+import { FaNode } from "react-icons/fa";
+import { SiStyledcomponents } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import { DiCss3 } from "react-icons/di";
+import { DiJsBadge } from "react-icons/di";
 import { SiTypescript } from "react-icons/si";
 import { FaDocker } from "react-icons/fa";
+import { SiPostgresql } from "react-icons/si";
 
 export const stackData = [
   {
     title: "HTML",
-    img: imgHTML,
+    img: FaHtml5,
   },
   {
     title: "CSS",
-    img: imgCSS,
+    img: DiCss3,
   },
   {
     title: "JS",
-    img: imgJS,
+    img: DiJsBadge,
   },
   {
     title: "Node JS",
-    img: imgNode,
+    img: FaNode,
   },
   {
     title: "Styled Components",
-    img: imgStyled,
+    img: SiStyledcomponents,
   },
   {
     title: "React",
-    img: imgReact,
+    img: FaReact,
   },
-  { title: "Vuejs", img: FaVuejs },
+  {title: "Python", img: FaPython},
   { title: "TypeScript", img: SiTypescript },
   { title: "Docker", img: FaDocker },
+  {title: "Python", img: SiPostgresql},
 ];

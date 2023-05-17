788d1d639ee70cdeac4fd71a63df9b168222ac76
awesomelucas <lucascostaschmidt@gmail.com>
2023-05-17 09:19:22 -0300

    att userData.ts

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,6 +1,6 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "samuelleao",
-  whatsappNumber: "+5541111111111",
+  nameUser: "Lucas Schmidt",
+  githubUser: "awesomelucas",
+  linkedinUser: "awesomelucas",
+  whatsappNumber: "+5547996886353",
 };
